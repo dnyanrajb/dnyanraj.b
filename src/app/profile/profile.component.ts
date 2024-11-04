97bb324fb8e0229ff1853a0c830c8b6741d83262
Dnyanraj <bdnyanraj.18cse.rymec@gmail.com>
2024-11-04 19:52:39 +0530

    added code for about tab

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from '../footer/footer.component';
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, AboutComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
